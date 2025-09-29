26c82f90ae143c682638388d87f08bb437279134
Piyush-0502 <piyushghorapade96@gmail.com>
2025-09-29 22:44:26 +0530

    Fixing background as video is not available on github

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -4,7 +4,7 @@ const Video = () => {
   return (
     <div className='h-full w-full'>
       {/* <video className='h-full w-full object-cover' autoPlay loop muted src='../../../public/homebg.mp4'></video> */}
-      <img className='h-full w-full object-cover' src='../../../public/background.jpg'/>
+      <img className='h-full w-full object-cover' src='background.jpg'/>
     </div>
   )
 }
